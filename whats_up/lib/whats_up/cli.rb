1294b45a6835b6937bcc7a6718b9c1a2c215cf64
Srija S <srijakumar7@gmail.com>
2018-07-30 15:53:55 +0000

    Updated CLI file to display each headline with an index

--- whats_up/lib/whats_up/cli.rb
+++ whats_up/lib/whats_up/cli.rb
@@ -5,38 +5,28 @@ class WhatsUp::CLI
   def start
       puts "What's up with your city?"
       puts ""
-
       #displays a list of headlines
-
       WhatsUp::Scraper.scrape_news
       display_headlines
   end
 
   def display_headlines
     puts "Here are the most read news headlines for today!"
-
+    puts ""
     news_array = WhatsUp::News.all
-
     news_array.each.with_index(1) do|news, index|
-      puts "#{index}. #{WhatsUp::News.title}"
-    end
-
+      puts "#{index}. #{news.title}"
+      end
     puts""
     puts "Please select a headline to read the article:"
-
     input = gets.strip
     index = input.to_i - 1
-
     news = WhatsUp::news.all[index]
-
     WhatsUp::Scraper.scrape_news_details(news)
-
     puts "Here is the article:"
     puts News.author
     puts News.description
-
     display_headlines
   end
-
 
 end
