e3da67f6e3ee6f47565069a8261e2888bd187135
Srija S <srijakumar7@gmail.com>
2018-08-01 01:42:16 +0000

    Added exit

--- whats_up/lib/whats_up/cli.rb
+++ whats_up/lib/whats_up/cli.rb
@@ -14,42 +14,48 @@ class WhatsUp::CLI
   def display_headlines
     puts "Here are the 5 most read news headlines from Washington Post for today!"
     puts ""
+    
     news_array = WhatsUp::News.all
     news_array.each.with_index(1) do|news, index|
       puts "#{index}. #{news.title}"
-      end
+    end
+    
+    display_articles
+    
+  end
+
+  def display_articles
     puts""
     puts "Please input 1-5 select a headline to read the article or input exit to quit the application"
+    
     input = gets.strip
-    index = input.to_i - 1
+    
+    if input != "exit"
+        index = input.to_i - 1
 
-    if (index<=0) || (input>=4) 
-      puts "Please input a number between 1 - 5"
+        if (index<0) || (index>4) 
+          display_articles
+        end
+      
+        #What if they enter a number less or greater than 5
+
+        news = WhatsUp::News.all[index]
+
+
+        if !news.author || !news.description
+          WhatsUp::Scraper.scrape_news_details(news)
+        end
+
+      #  WhatsUp::Scraper.scrape_news_details(news)
+        puts ""
+        puts "Here is the article:"
+        puts news.title
+        puts ""
+        puts "By: " + news.author
+        puts ""
+        puts news.description
+        display_headlines
     end
-
-    if input=="exit"
-      exit
-    end
-    #What if they enter a number less or greater than 5
-
-    news = WhatsUp::News.all[index]
-
-
-    if !news.author || !news.description
-      WhatsUp::Scraper.scrape_news_details(news)
-    end
-
-  #  WhatsUp::Scraper.scrape_news_details(news)
-    puts ""
-    puts "Here is the article:"
-    puts news.title
-    puts ""
-    puts "By: " + news.author
-    puts ""
-    puts news.description
-    display_headlines
-
-  
   end
 
 #Build a method to exit out the program if user types in  "exit"
